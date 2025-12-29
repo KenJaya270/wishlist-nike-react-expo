9656ff3e3d3145faea9d2a74c1b6bb1d3f107ad4
KenJaya270 <kenjayakusuma123@gmail.com>
2025-12-29 21:40:54 +0800

    feat: Add Supabase client configuration and credentials to `app.json`.

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -6,8 +6,13 @@ import { Database } from '../types/database.types';
 
 // const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || '';
 // const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || '';
+
+
+//tambahkan pada app.json
+//pada extra: {} tambahkan "supabaseUrl" dan "supabaseAnonKey"
 const supabaseUrl = Constants.expoConfig?.extra?.supabaseUrl;
 const supabaseAnonKey = Constants.expoConfig?.extra?.supabaseAnonKey;
+
 
 if (!supabaseUrl || !supabaseAnonKey) {
     console.warn('⚠️ Supabase credentials not found. Please check your .env file.');
