ffab03ae7d63c4a54b0e1f4170646874e674bedc
KenJaya270 <kenjayakusuma123@gmail.com>
2025-12-29 12:27:53 +0800

    setup deffered link

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,16 @@
+import Airbridge from "airbridge-react-native-sdk";
 import { Stack } from "expo-router";
+import { useEffect } from "react";
 
 export default function Layout() {
+  useEffect(() => {
+    if (Airbridge && (Airbridge as any).init) {
+      (Airbridge as any).init("wishlistapp", "1e44ab047e4141e1b89f109015e8c44c");
+    } else {
+      console.log("Airbridge is not available");
+    }
+  }, []);
+
   return (
     <Stack
       screenOptions={{
